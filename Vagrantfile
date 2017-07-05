3425530b7bd7b3ebde05f30db0ed971cc554314b
Holger Woltersdorf <hw@hollo.me>
2017-07-05 22:24:42 +0200

    Adds image upload and thumbnail functionality

--- Vagrantfile
+++ Vagrantfile
@@ -5,6 +5,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "DreiWolt/devops007"
   config.vm.network :private_network, ip: "192.168.3.13"
   config.vm.hostname = "CCOne"
+  config.vm.synced_folder ".", "/vagrant", type: "nfs"
   config.hostsupdater.aliases = ["dev.c-und-c.one", "pma.c-und-c.one", "readis.c-und-c.one"]
 
   config.vm.provision "shell", path: "env/bootstrap.sh", run: "always"
--- src/Application/Web/Upload/Write/UploadImagesRequestHandler.php
+++ src/Application/Web/Upload/Write/UploadImagesRequestHandler.php
@@ -7,7 +7,9 @@ namespace hollodotme\CCOne\Application\Web\Upload\Write;
 
 use hollodotme\CCOne\Traits\EnvInjecting;
 use IceHawk\IceHawk\Interfaces\HandlesPostRequest;
+use IceHawk\IceHawk\Interfaces\ProvidesUploadedFileData;
 use IceHawk\IceHawk\Interfaces\ProvidesWriteRequestData;
+use Intervention\Image\ImageManager;
 
 /**
  * Class UploadImagesRequestHandler
@@ -19,6 +21,68 @@ final class UploadImagesRequestHandler implements HandlesPostRequest
 
 	public function handle( ProvidesWriteRequestData $request ) : void
 	{
+		$input = $request->getInput();
+		$files = $input->getFiles( 'files' );
+
+		$countSaved = 0;
+
+		foreach ( $files as $file )
+		{
+			if ( !$file->didUploadSucceed() )
+			{
+				continue;
+			}
+
+			$fileName   = $this->getFileName( $file );
+			$targetPath = dirname( __DIR__, 5 ) . '/public/media/' . $fileName;
+
+			if ( !copy( $file->getTmpName(), $targetPath ) )
+			{
+				continue;
+			}
+
+			$this->createThumbnail( $file, $fileName );
+
+			++$countSaved;
+		}
+
+		if ( $countSaved === 0 )
+		{
+			die( json_encode( [ 'success' => false, 'error' => 'Konnte Bilder nicht hochladen.' ] ) );
+		}
+
 		die( json_encode( [ 'success' => true, 'error' => '' ] ) );
+	}
+
+	private function getFileName( ProvidesUploadedFileData $file ) : string
+	{
+		$uuid = bin2hex( random_bytes( 16 ) );
+
+		return strtolower( preg_replace( '#^.+\.(jpe?g|png|gif)$#i', "{$uuid}.$1", $file->getName() ) );
+	}
+
+	private function createThumbnail( ProvidesUploadedFileData $file, string $fileName ) : void
+	{
+		$image = (new ImageManager())
+			->make( $file->getTmpName() )
+			->widen(
+				300,
+				function ( $constraint )
+				{
+					$constraint->aspectRatio();
+					$constraint->upsize();
+				}
+			)
+			->heighten(
+				300,
+				function ( $constraint )
+				{
+					$constraint->aspectRatio();
+					$constraint->upsize();
+				}
+			)
+			->resizeCanvas( 300, 300, 'center', false, '#fff6ef' );
+
+		$image->save( dirname( __DIR__, 5 ) . '/public/media/thumbs/' . $fileName );
 	}
 }
